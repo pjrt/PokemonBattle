8abd72c65dae10c9c7f9d92c054bf962c70e3054
Pedro Rodriguez <P.J.Rodriguez.T@gmail.com>
2014-07-26 15:26:23 -0400

    Remove client, types and resource. Use Haskmon
    Remove all the built in client stuff and use Haskmon instead.

--- src/Main.hs
+++ src/Main.hs
@@ -1,37 +1,33 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
-import Types
-import Client
+import Haskmon
 import System.Random (newStdGen, randomRIO)
 import System.Random.Shuffle (shuffle', shuffleM)
 
 import Data.Word
 
-import Control.Applicative((<$>))
+import Control.Applicative
 import Control.Monad.Random
 import Control.Monad.Writer
-import Control.Applicative
+import qualified Data.Traversable as T
 
-choose :: (RandomGen g, Monad m, Functor m) => [a] -> RandT g m a
-choose xs = head <$> shuffleM xs
+-- | App type. A composition of Random, Writer and some other monad (IO)
+newtype PB g m a = PB { runPB :: RandT g (WriterT [String] m) a }
+                   deriving (Functor, Applicative, Monad, MonadIO, MonadRandom, MonadWriter [String])
 
 -- Get a random pokemon from the original 152
-getRandomPkmn :: RandomGen g => Rand g (IO Pokemon)
+getRandomPkmn :: (RandomGen g, MonadIO m, Functor m) => PB g m Pokemon
 getRandomPkmn = do
             randomInt <- getRandomR(1, 152)
-            return $ getPokemonById randomInt
+            liftIO $ getPokemonById randomInt
 
--- Given a pokemon, return it with 4 randomly downloaded moves
-downloadMoves :: Pokemon -> IO Pokemon
-downloadMoves pkm @ (Pokemon { moves = PotentialMoves funcGetters }) = do
-            let randMoves = shuffleM funcGetters -- Randomize the getters
-            moves <- sequence =<< take 4 <$> randMoves -- Pick 4 from the random getters and download them
-            return $ pkm { moves = Moves moves } -- Return the same pokemon with the downloaded moves
-downloadMoves pkm = return pkm -- If moves already downloaded, then just get them
-
-getReadyRandomPkm :: RandomGen g => g -> IO Pokemon
-getReadyRandomPkm gen = (evalRand getRandomPkmn gen) >>= downloadMoves
+-- Given a pokemon, get 4 random moves from it
+downloadMoves :: (RandomGen g, MonadIO m) => Pokemon -> PB g m [Move]
+downloadMoves pkm = do
+            metaMoves <- return $ pokemonMoves pkm
+            randMoves <- shuffleM metaMoves -- Randomize the getters
+            liftIO $ (mapM getMove $ take 4 randMoves) -- Pick 4 from the random getters and download them
 
 data DamageResult = Damage Int | Missed
 instance Show DamageResult where
@@ -40,31 +36,35 @@ instance Show DamageResult where
 
 calculateDamage :: (RandomGen g, Monad m, Functor m) => Pokemon -> Move -> RandT g m DamageResult
 calculateDamage  pkmn move = do
-            let top = floor $ fromIntegral ((attack pkmn + spAtk pkmn) * power move) / 300
+            let top = (floor $ fromIntegral ((pokemonAttack pkmn + pokemonSpAtk pkmn) * movePower move) / 300)
             pkmAtkDmg <- getRandomR (0, top)
-            didItHit <- (>= accuracy move) <$> getRandomR (0, 200)
-            if didItHit then return $ Damage pkmAtkDmg
-                        else return Missed
+            didItHit <- (>= moveAccuracy move) <$> getRandomR (0, 200)
+            return $ if didItHit then Damage pkmAtkDmg else Missed
 
 main = do
   gen <- newStdGen
-  pk <- getReadyRandomPkm gen
-  putStrLn $ "A wild " ++ pokemonName pk ++ " has appeared!"
-  let ms = case moves pk of { Moves m -> m }
-      bRes = battle 100 0 pk ms
-      reports = execWriter $ evalRandT bRes gen
-  mapM_ putStrLn reports
+  battleReport <- execWriterT (evalRandT (runPB go) gen)
+  mapM_ putStrLn battleReport
+    where go :: (RandomGen g, Monad m, Functor m, MonadIO m) => PB g m ()
+          go = do
+              pk <- getRandomPkmn
+              moves <- downloadMoves pk
+              playGame pk moves
 
 type HP = Int
 type MoveCount = Int
 type MoveSet = [Move]
-type WriterRand g a = RandT g (Writer [String]) a
 
-battle :: RandomGen g => HP -> MoveCount -> Pokemon -> MoveSet -> WriterRand g ()
-battle hp count pkm moves = battle' hp count
+playGame :: (RandomGen g, Monad m, Functor m) => Pokemon -> MoveSet -> PB g m ()
+playGame pk moves = do
+  tell $ ["A wild " ++ pokemonName pk ++ " has appeared!"]
+  battle 100 0 pk moves
+
+battle :: (RandomGen g, Monad m, Functor m) => HP -> MoveCount -> Pokemon -> MoveSet -> PB g m ()
+battle hp count pkm moves = PB (battle' hp count)
   where
     battle' hp count =
-                    if hp <= 0 then tell ["You were defeated in " ++ (show count) ++ " moves!"]
+                    if hp <= 0 then tell ["You were defeated in " ++ show count ++ " moves!"]
                     else do
                       move <- choose moves
                       dmg <- calculateDamage pkm move
@@ -74,3 +74,8 @@ battle hp count pkm moves = battle' hp count
                                 calculateHp hp' (Damage dmg) = hp' - dmg
                                 printResult pkName mvName dmg =
                                     pkName ++ " attacked you with " ++ mvName ++ " " ++ show dmg
+
+-- Utility for choosing a move from a MoveSet
+choose :: (RandomGen g, Monad m, Functor m) => [a] -> RandT g m a
+choose xs = head <$> shuffleM xs
+
