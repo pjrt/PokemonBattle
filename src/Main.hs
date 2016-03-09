307b4d7d81dae7e7d8525c6ab93a31d30190b887
Pedro Rodriguez <pedro@pjrt.co>
2016-03-09 04:37:42 +0000

    No-op: Remove useless stuff and more formatting
    There is no need for the generic `RandomGen g`, so use plain StdGen

--- src/Main.hs
+++ src/Main.hs
@@ -8,17 +8,17 @@ import Control.Monad.Random
 import Control.Monad.Writer
 
 -- | App type. A composition of Random, Writer and some other monad (IO)
-newtype PB g m a = PB { runPB :: RandT g (WriterT [String] m) a }
-                   deriving (Functor, Applicative, Monad, MonadIO, MonadRandom, MonadWriter [String])
+newtype PB m a = PB { runPB :: RandT StdGen (WriterT [String] m) a }
+  deriving (Functor, Applicative, Monad, MonadIO, MonadRandom, MonadWriter [String])
 
 -- Get a random pokemon from the original 152
-getRandomPkmn :: (RandomGen g, MonadIO m, Functor m) => PB g m Pokemon
+getRandomPkmn :: MonadIO m => PB m Pokemon
 getRandomPkmn = do
             randomInt <- getRandomR (1, 152)
             liftIO $ getPokemonById randomInt
 
 -- Given a pokemon, get 4 random moves from it
-downloadMoves :: (RandomGen g, MonadIO m) => Pokemon -> PB g m [Move]
+downloadMoves :: MonadIO m => Pokemon -> PB m [Move]
 downloadMoves pkm = do
             let metaMoves = pokemonMoves pkm
             randMoves <- shuffleM metaMoves -- Randomize the getters
@@ -27,36 +27,46 @@ downloadMoves pkm = do
 data DamageResult = Damage Int | Missed
 instance Show DamageResult where
         show (Damage amt) = "dealing " ++ show amt ++ " damage!"
-        show (Missed) = "but it missed!"
+        show Missed = "but it missed!"
 
-calculateDamage :: (RandomGen g, Monad m, Functor m) => Pokemon -> Move -> RandT g m DamageResult
-calculateDamage  pkmn move = do
-            let top = floor $ fromIntegral ((pokemonAttack pkmn + pokemonSpAtk pkmn) * movePower move) / 300
+-- | Given a pokemin and a move, calculate, randomly, how much damage that
+-- move will do.
+calculateDamage :: (RandomGen g, Monad m) => Pokemon -> Move -> RandT g m DamageResult
+calculateDamage pkmn move = do
+            let pkAtk  = (+) <$> pokemonAttack <*> pokemonSpAtk $ pkmn
+                top = floor (fromIntegral (pkAtk * movePower move) / 300 :: Double)
             pkmAtkDmg <- getRandomR (0, top)
+            -- TODO: We are comparing it from 0 to 200 in order to make it
+            -- more likely to hit. Though what we should do is implement
+            -- a pseudo-random system instead of purely random. Purely random
+            -- is too boring.
             didItHit <- (>= moveAccuracy move) <$> getRandomR (0, 200)
             return $ if didItHit then Damage pkmAtkDmg else Missed
 
+main :: IO ()
 main = do
   gen <- newStdGen
   battleReport <- execWriterT (evalRandT (runPB go) gen)
   mapM_ putStrLn battleReport
-    where go :: (RandomGen g, Monad m, Functor m, MonadIO m) => PB g m ()
-          go = do
-              pk <- getRandomPkmn
-              moves <- downloadMoves pk
-              playGame pk moves
+    where go = do pk <- getRandomPkmn
+                  moves <- downloadMoves pk
+                  playGame pk moves
 
 type HP = Int
 type MoveCount = Int
 type MoveSet = [Move]
 
-playGame :: (RandomGen g, Monad m, Functor m) => Pokemon -> MoveSet -> PB g m ()
+playGame :: Monad m => Pokemon -> MoveSet -> PB m ()
 playGame pk moves = do
   tell ["A wild " ++ pokemonName pk ++ " has appeared!"]
   battle 100 0 pk moves
 
-battle :: (RandomGen g, Monad m, Functor m) => HP -> MoveCount -> Pokemon -> MoveSet -> PB g m ()
-battle hp count pkm moves = PB (battle' hp count)
+-- | A battle consists of an initial HP (your HP), a count, a pokemon and
+-- a moveset from said pokemon. The pokemon will continusly attack you with
+-- one of the moves from the moveset until you die, counting how many moves
+-- it took.
+battle :: Monad m => HP -> MoveCount -> Pokemon -> MoveSet -> PB m ()
+battle h c pkm moves = PB $ battle' h c
   where
     battle' hp count =
                     if hp <= 0 then tell ["You were defeated in " ++ show count ++ " moves!"]
